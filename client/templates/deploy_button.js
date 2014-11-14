0ae86e01dc25463d5fa44b1fb997a56086bdd24b
Sören Ohnmeiss <soren.ohnmeiss@netapsys.fr>
2014-11-14 21:06:03 +0100

    fix Giorgiolino/kideploy#1

--- client/templates/deploy_button.js
+++ client/templates/deploy_button.js
@@ -1,38 +1,43 @@
-  Template.deployButton.events({
-    'click .trigger-deploy-start': function (event, template) {
-      // increment the counter when button is clicked
-	
-	if (confirm("Y a vraiment pas le choix ?")) {
-	      	Session.set("counter", Session.get("counter") + 1);
-		var me = $(event.target);
-		me.addClass('trigger-deploy-stop');
-		me.removeClass('trigger-deploy-start');
-		Deploys.insert({type : "start", createdAt: new Date(), actor : Meteor.user()});
-		
-	} else {
-		return false;
-	}
-    },
-    'click .trigger-deploy-stop': function (event, template) {
-      // increment the counter when button is clicked
+Template.deployButton.helpers({
+    buttonStatusClass: function () {
+        if (Meteor.user().profile.isDeploying) {
+            return 'trigger-deploy-stop';
+        }
+        return 'trigger-deploy-start';
+    }
+});
 
-        if (confirm("On est d'accord que c'est bien fini ?")) {
-              	Session.set("counter", Session.get("counter") + 1);
-                var me = $(event.target);
-                me.addClass('trigger-deploy-start');
-                me.removeClass('trigger-deploy-stop');
-		Deploys.insert({type : "stop", createdAt: new Date(), actor : Meteor.user()});
+Template.deployButton.events({
+    'click .trigger-deploy-start': function (event, template) {
+        // increment the counter when button is clicked
+
+        if (confirm("Y a vraiment pas le choix ?")) {
+            Session.set("counter", Session.get("counter") + 1);
+            Meteor.users.update(Meteor.userId(), {$set: {"profile.isDeploying": true}});
+            Deploys.insert({type: "start", createdAt: new Date(), actor: Meteor.user()});
 
         } else {
-                return false;
+            return false;
+        }
+    },
+    'click .trigger-deploy-stop': function (event, template) {
+        // increment the counter when button is clicked
+
+        if (confirm("On est d'accord que c'est bien fini ?")) {
+            Session.set("counter", Session.get("counter") + 1);
+            Meteor.users.update(Meteor.userId(), {$set: {"profile.isDeploying": false}});
+            Deploys.insert({type: "stop", createdAt: new Date(), actor: Meteor.user()});
+
+        } else {
+            return false;
         }
     },
 
-    'mouseenter button.action': function(event, template) {
-	$(event.target).addClass('animated shake');
+    'mouseenter button.action': function (event, template) {
+        $(event.target).addClass('animated shake');
     },
-    'mouseleave button.action': function(event, template) {
+    'mouseleave button.action': function (event, template) {
         $(event.target).removeClass('animated shake');
     }
-  });
+});
 
